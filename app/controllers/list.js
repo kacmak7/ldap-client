4ab6df7434357dbf72c8f8b176314c40b499b4d9
Kacper Makuch <kacbull007@gmail.com>
2018-11-07 07:48:59 +0000

    Enable list of accounts

--- app/controllers/list.js
+++ app/controllers/list.js
@@ -1,3 +1,9 @@
 app.controller("listCtrl", function($scope, $http) {
     $scope.active = true;
+
+    $http.get("http://localhost:8080/authors")
+    .then(function(response) {
+        console.log("getAuthors()");
+        $scope.accounts = response.data.authors;
+    });
 })
