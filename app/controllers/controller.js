e9cc46fe007f4eeb10a9c31737c13046f11a7091
Kacper Makuch <kacbull007@gmail.com>
2018-12-04 14:04:18 +0000

    Start building csv generator

--- app/controllers/controller.js
+++ app/controllers/controller.js
@@ -1,3 +1,11 @@
 app.controller("ctrl", function($scope, $http) {
-    
+    $scope.generateCsv = function() {
+        $http.get("http://accounts.zipper.release11.com/api/users")
+        .then(function(response) {
+            console.log("count: " + response.data.count);
+            var users = response.data;
+            delete $scope.users.count;
+        });
+        var usersJson = JSON.stringify(users);
+    }
 })
